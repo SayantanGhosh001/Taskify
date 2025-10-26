48c882bcf717c2d57fe6cb3f353f4546d42d8e7c
Sayantan Ghosh <thesayantanghosh@gmail.com>
2025-10-27 01:57:06 +0530

    Refactor import path for AuthContext in ProtectedRoute

--- Frontend/src/components/ProtectedRoute.jsx
+++ Frontend/src/components/ProtectedRoute.jsx
@@ -1,5 +1,5 @@
 import { Navigate, Outlet } from "react-router-dom";
-import { useAuth } from "../context/AuthContext";
+import { useAuth } from "../context/authContext";
 
 const ProtectedRoute = () => {
   const { user } = useAuth();
@@ -8,3 +8,4 @@ const ProtectedRoute = () => {
 };
 
 export default ProtectedRoute;
+
