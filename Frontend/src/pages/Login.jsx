5350a698885b9d7f367cf7aa1d733b8c0e998ca5
Sayantan Ghosh <thesayantanghosh@gmail.com>
2025-10-27 01:57:45 +0530

    Fix import path for AuthContext in Login component

--- Frontend/src/pages/Login.jsx
+++ Frontend/src/pages/Login.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { useAuth } from "../context/AuthContext";
+import { useAuth } from "../context/authContext";
 import Navbar from "../components/Navbar";
 import "../App.css";
 
@@ -49,3 +49,4 @@ const Login = () => {
 };
 
 export default Login;
+
